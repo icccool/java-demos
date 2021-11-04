916c97e5537c32272142b6d3bdf37fcecd04856e
wangjibo <wangjibo@hydee.com>
2021-11-04 08:35:11 +0800

    transform

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 1. 解决无法提交github  
 (1) 本地开代理  
-(2) git  
+(2) 修改git配置文件 .git/config文件增加以下内容  
 ```
 [http]
 proxy = socks5://127.0.0.1:7890
--- nettty-demo/src/main/java/com/demos/netty/httpfileserver/HttpFileServer.java
+++ nettty-demo/src/main/java/com/demos/netty/httpfileserver/HttpFileServer.java
@@ -1,6 +1,45 @@
 package com.demos.netty.httpfileserver;
 
+
+import com.demos.netty.timserver3.TimeServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
 public class HttpFileServer {
 
+    public static void main(String[] args) {
+
+    }
+
+    public static void bind(int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, childGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel channel) throws Exception {
+                            channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            channel.pipeline().addLast(new StringDecoder());
+                            channel.pipeline().addLast(new TimeServerHandler());
+                        }
+                    });
+
+            //绑定端口,同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+
+        }
+    }
 
 }

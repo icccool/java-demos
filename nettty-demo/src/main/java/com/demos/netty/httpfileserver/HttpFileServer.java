62f81f43f654667381ab1f25d568e1cf9966ffd7
wangjibo <wangjibo@hydee.com>
2021-11-11 23:04:39 +0800

    HttpFileServer完成！

--- nettty-demo/src/main/java/com/demos/netty/httpfileserver/HttpFileServer.java
+++ nettty-demo/src/main/java/com/demos/netty/httpfileserver/HttpFileServer.java
@@ -1,44 +1,56 @@
 package com.demos.netty.httpfileserver;
 
-
 import com.demos.netty.timserver3.TimeServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class HttpFileServer {
 
-    public static void main(String[] args) {
+    //默认当前路径
+    private static final String URL = "/";
 
+    public static void main(String[] args) throws InterruptedException {
+        new HttpFileServer().bind(8099, URL);
     }
 
-    public static void bind(int port) throws InterruptedException {
+    public void bind(int port, String url) throws InterruptedException {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup childGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, childGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 1024)
-                    .childHandler(new ChannelInitializer() {
-                        @Override
-                        protected void initChannel(Channel channel) throws Exception {
-                            channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
-                            channel.pipeline().addLast(new StringDecoder());
-                            channel.pipeline().addLast(new TimeServerHandler());
-                        }
-                    });
-
-            //绑定端口,同步等待成功
-            ChannelFuture f = b.bind(port).sync();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                    ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                    // 新增HTTP响应编码器，对HTTP响应消息进行编码
+                    ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                    // 新增Chunked handler，主要作用是支持异步发送大的码流（例如大文件传输）
+                    // 但是不占用过多的内存，防止发生java内存溢出错误
+                    ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                    // HttpFileServerHandler用于文件服务器的业务逻辑处理
+                    ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                }
+            });
+            String host = "127.0.0.1";
+            ChannelFuture future = b.bind(host, port).sync();
+            System.out.println("HTTP文件目录服务器启动，网址是 : http://" + host + ":" + port + url);
             //等待服务端监听端口关闭
-            f.channel().closeFuture().sync();
+            future.channel().closeFuture().sync();
         } finally {
-
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
 
---
+++ nettty-demo/src/main/java/com/demos/netty/httpfileserver/README.md
@@ -0,0 +1,4 @@
+#### Netty基础入门使用
+
+增加httpFileServer
+参考： https://blog.csdn.net/qq_37598011/article/details/83422909

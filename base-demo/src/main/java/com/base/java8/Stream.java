b51dc55e2f333afd15a002b7a0d731bcbe3051cb
wangjibo <wangjibo@tianji.cn>
2021-11-12 11:07:21 +0800

    一些示例

---
+++ base-demo/src/main/java/com/base/java8/Stream.java
@@ -0,0 +1,15 @@
+package com.base.java8;
+
+public class Stream {
+
+    public static void main(String[] args) {
+        //        blackInfoDTOS.forEach(e -> pageAnchorShowingList
+//                .stream()
+//                .filter(anchorShowInfo -> e.getUserId() == anchorShowInfo.getAnchorId())
+//                .findFirst()
+//                .ifPresent(t->{
+//                    t.setGroupId(e.getGroupId());
+//                    //
+//                });
+    }
+}
